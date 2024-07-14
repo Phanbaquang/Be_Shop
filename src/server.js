48881aa852a96da91005e31f4e22d5ad85e8cbcf
nguyenthanhdat <nguyend@digitalplatformer.co.j>
2024-07-14 15:16:31 +0700

    update payment

--- src/server.js
+++ src/server.js
@@ -6,17 +6,40 @@ const { app, server } = require('./sockets/socket')
 const dotenv = require('dotenv')
 const path = require('path')
 const cors = require('cors')
+const { createOrder, captureOrder } = require('./setupPaypal')
 // const app = express()
 dotenv.config()
 
 app.use(cors())
 app.options('*', cors())
 const hostname = 'localhost'
-const port = 8000
+const port = 8017
 app.use(bodyParser.json())
 connectDb()
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use('', require('./routes/v1/index'))
+app.post('/api/orders', async (req, res) => {
+  try {
+    console.log('namdz')
+    const { cart } = req.body
+    const { jsonResponse, httpStatusCode } = await createOrder(cart)
+    res.status(httpStatusCode).json(jsonResponse)
+  } catch (error) {
+    console.error('Failed to create order:', error)
+    res.status(500).json({ error: 'Failed to create order.' })
+  }
+})
+
+app.post('/api/orders/:orderID/capture', async (req, res) => {
+  try {
+    const { orderID } = req.params
+    const { jsonResponse, httpStatusCode } = await captureOrder(orderID)
+    res.status(httpStatusCode).json(jsonResponse)
+  } catch (error) {
+    console.error('Failed to create order:', error)
+    res.status(500).json({ error: 'Failed to capture order.' })
+  }
+})
 app.get('/', (req, res) => {
   res.end('<h1>Hello World!</h1><hr>')
 })
